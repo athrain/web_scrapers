f768112bc3034dda1fc1aa05964f5ddc2a2a5b0a
Dawid <dawidniewdana@gmail.com>
2023-07-08 22:20:00 +0200

    Some ugly code that needs to be checked - for learning purposes

--- Web scraper - basics.py
+++ Web scraper - basics.py
@@ -8,7 +8,7 @@ from urllib.error import URLError #to handle error when there is no connection t
 #html = urlopen('http://www.pythonscraping.com/pages/page1.html')
 #html = urlopen('https://nofluffjobs.com/pl/job/senior-azure-devops-engineer-xebia-remote')
 
-html = urlopen('https://nofluffjobs.com/pl')
+html = urlopen('https://nofluffjobs.com/pl/Java?page=1')
 
 
 bs = BeautifulSoup(html.read(), 'html.parser')
@@ -18,12 +18,12 @@ object = 'head'
 #print(bs.h1) #prints only H1
 print(bs)
 
-
-try:
- html = urlopen('https://pythonscrapingthisurldoesnotexist.com')
-except HTTPError as e:
- print(e)
-except URLError as e:
- print('The server could not be found!')
-else:
- print('It Worked!')
+# below - some line to see the errors while loading a page
+# try:
+#  html = urlopen('https://pythonscrapingthisurldoesnotexist.com')
+# except HTTPError as e:
+#  print(e)
+# except URLError as e:
+#  print('The server could not be found!')
+# else:
+#  print('It Worked!')
